d03082a041615fbf45b94aafa570fd8d72bb8818
zonelearningattendance <zonelearningattendance@gmail.com>
2026-08-31 16:55:46 -0400

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'dc10c3cd7d0d4c2e9d07e5d2509f21c6',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
