15ce2c51560bc8d66511a446e9b1ccdd7d98b9eb
ZONplus <zonelearningattendance@gmail.com>
2026-09-01 11:00:34 -0400

    Update PageSocial.tsx

--- components/PageSocial.tsx
+++ components/PageSocial.tsx
@@ -1,37 +1,9 @@
 'use client'
 
-import React, { useEffect, useRef } from 'react'
+import React from 'react'
 
 export const PageSocial: React.FC = () => {
-  const ref = useRef<HTMLDivElement>(null)
-
-  useEffect(() => {
-    if (!ref.current || ref.current.hasChildNodes()) return
-
-    const script = document.createElement('script')
-    script.src = 'https://giscus.app/client.js'
-    script.setAttribute('data-repo', 'zonplus/nextjs-notion-starter-kit')
-    script.setAttribute('data-repo-id', 'R_kgDOUKLABg')
-    script.setAttribute('data-category', 'General')
-    script.setAttribute('data-category-id', 'DIC_kwDOUKLABs4DEp2U')
-    script.setAttribute('data-mapping', 'pathname')
-    script.setAttribute('data-strict', '0')
-    script.setAttribute('data-reactions-enabled', '1')
-    script.setAttribute('data-emit-metadata', '0')
-    script.setAttribute('data-input-position', 'bottom')
-    script.setAttribute('data-theme', 'preferred_color_scheme')
-    script.setAttribute('data-lang', 'en')
-    script.setAttribute('crossOrigin', 'anonymous')
-    script.async = true
-
-    ref.current.appendChild(script)
-  }, [])
-
-  return (
-    <div style={{ marginTop: '4rem', width: '100%' }}>
-      <div ref={ref} />
-    </div>
-  )
+  return null
 }
 
 export default PageSocial
