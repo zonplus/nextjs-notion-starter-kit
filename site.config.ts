541194652e94af87ef2a60bc5925526bd2b3326d
ZONplus <zonelearningattendance@gmail.com>
2026-09-01 10:28:54 -0400

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -1,33 +1,41 @@
 import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
+  // the site's root Notion page (required)
   rootNotionPageId: '3cdee6ae553b80c79e80e904ec1992ae',
+
+  // if you want to restrict pages to a single notion workspace (optional)
   rootNotionSpaceId: null,
 
+  // basic site info (required)
   name: 'ZONplus Circles',
   domain: 'nextjs-notion-starter-kit-ochre-ten-27.vercel.app',
   author: '',
 
+  // open graph metadata (optional)
   description: 'ZONplus Community Hub and Resources',
 
+  // social usernames
   twitter: '',
   github: '',
   linkedin: '',
   newsletter: '',
   youtube: '',
 
+  // default notion icon and cover images for site-wide consistency (optional)
   defaultPageIcon: null,
   defaultPageCover: null,
   defaultPageCoverPosition: 0.5,
 
+  // whether or not to enable support for LQIP preview images (optional)
   isPreviewImageSupportEnabled: true,
-  isRedisEnabled: false,
-  pageUrlOverrides: null,
-  navigationStyle: 'default',
 
-  // Giscus configuration
-  giscusRepo: 'zonplus/nextjs-notion-starter-kit',
-  giscusRepoId: 'PASTE_YOUR_REPO_ID_HERE',
-  giscusCategory: 'General',
-  giscusCategoryId: 'PASTE_YOUR_CATEGORY_ID_HERE'
+  // whether or not redis is enabled for caching generated preview images (optional)
+  isRedisEnabled: false,
+
+  // map of notion page IDs to URL paths (optional)
+  pageUrlOverrides: null,
+
+  // navigation style
+  navigationStyle: 'default'
 })
