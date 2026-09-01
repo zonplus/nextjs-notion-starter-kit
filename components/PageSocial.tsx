6721e4b4fb68a34faf1b95b3018e18718645db0f
ZONplus <zonelearningattendance@gmail.com>
2026-09-01 10:32:40 -0400

    Update PageSocial.tsx

--- components/PageSocial.tsx
+++ components/PageSocial.tsx
@@ -1,5 +1,25 @@
 import React from 'react'
+import Giscus from '@giscus/react'
 
 export const PageSocial: React.FC = () => {
-  return null
+  return (
+    <div style={{ marginTop: '4rem', width: '100%' }}>
+      <Giscus
+        id="comments"
+        repo="zonplus/nextjs-notion-starter-kit"
+        repoId="PASTE_YOUR_REPO_ID_HERE"
+        category="General"
+        categoryId="PASTE_YOUR_CATEGORY_ID_HERE"
+        mapping="pathname"
+        reactionsEnabled="1"
+        emitMetadata="0"
+        inputPosition="top"
+        theme="light"
+        lang="en"
+        loading="lazy"
+      />
+    </div>
+  )
 }
+
+export default PageSocial
