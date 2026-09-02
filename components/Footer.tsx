f0c478ea4eda7e6bdbe1bb17e69033bbfaf77203
ZONplus <zonelearningattendance@gmail.com>
2026-09-02 19:45:53 -0400

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,17 @@
 import * as React from 'react'
 import styles from './styles.module.css'
 
+const footerStyle = {
+  width: '100%',
+  maxWidth: 'var(--notion-max-width, 720px)',
+  marginLeft: 'auto',
+  marginRight: 'auto',
+  paddingLeft: 'max(calc(2vw - 8px), 0px)',
+  paddingRight: 'max(calc(2vw - 8px), 0px)'
+}
+
+const copyrightStyle = { paddingLeft: '8px' }
+
 const linkStyle = {
   display: 'flex',
   alignItems: 'center',
@@ -13,8 +24,8 @@ const iconStyle = { opacity: 0.85 }
 
 export function FooterImpl() {
   return (
-    <footer className={styles.footer}>
-      <div className={styles.copyright}>Made with 😊</div>
+    <footer className={styles.footer} style={footerStyle}>
+      <div className={styles.copyright} style={copyrightStyle}>Made with 😊</div>
       <div className={styles.social}>
         <a href='https://www.instagram.com/tmuzonplus/' title='Instagram @tmuzonplus' target='_blank' rel='noopener noreferrer' style={linkStyle}>
           <svg width='16' height='16' viewBox='0 0 24 24' fill='none' stroke='currentColor' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round' style={iconStyle}>
@@ -24,8 +35,3 @@ export function FooterImpl() {
           </svg>
         </a>
       </div>
-    </footer>
-  )
-}
-
-export const Footer = React.memo(FooterImpl)
